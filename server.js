4fd36d1ec940040caadfc5ba3602704899d81c58
Cool Droid <cooldroid7@gmail.com>
2018-11-03 13:12:42 +0530

    modules added, initial boilerplate template

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { express } = require('./globals');
+const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const path = require('path');
